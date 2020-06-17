2141a0172611e442de0f35b7ca8af10f5269b212
srahmed <srehanpk@gmail.com>
2020-06-17 23:01:08 +0500

    Update CSS

--- src/Input.js
+++ src/Input.js
@@ -25,11 +25,11 @@ function Input  () {
                 <div className="plus">
 
                 <form >
-                    <b>Description</b><br/>
+                    <b>Description:</b>
                     <input type = "text" value={incText} onChange={(e)=>setIncText(e.target.value)}  placeholder="input text" ></input>
   
-                    <br/>
-                    <b>Amount</b><br/>
+                    <br/><br/>
+                    <b>Amount:</b>
                     <input type = "text" value={incAmount} onChange={(e)=>setIncAmount(e.target.value)} placeholder="Input Amount" ></input>
                     <br/><br/>
                     <button className="plus-btn">Add Income</button>
@@ -40,11 +40,11 @@ function Input  () {
                 <div className="minus"> 
 
                 <form>
-                    <b>Description</b><br/>
+                    <b>Description:</b>
                     <input type = "text" value={expText} onChange={(e)=>setExpText(e.target.value)} placeholder="input text" ></input>
 
-                    <br/> 
-                    <b>Amount</b><br/>
+                    <br/> <br/>
+                    <b>Amount:</b>
                     <input type = "text" value={expAmount} onChange={(e)=>setExpAmount(e.target.value)} placeholder="Input Amount" ></input>
                     <br/><br/>
                     <button className="minus-btn">Add Expense</button>
@@ -56,6 +56,7 @@ function Input  () {
                 <div className="form">
                     <div className="plus">
                     <h3>History</h3>
+
                     <ul className="list">
                     {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
                     
