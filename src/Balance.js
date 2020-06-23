7f9057932a4732fe09187695540b3a6004868141
srahmed <srehanpk@gmail.com>
2020-06-23 12:19:20 +0500

    Update Balance.js

--- src/Balance.js
+++ src/Balance.js
@@ -5,7 +5,7 @@ function Balance() {
   const { transaction } = useContext(GlobalContext);
   const amounts = transaction.map((transaction) => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const total = amounts.reduce((acc, item) => (acc += item), 0);
 
   return (
     <div className="balance">
